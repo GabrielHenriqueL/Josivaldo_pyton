e3db113b87f9a6491e37533980ea6df94cf9667f
GabrielHenriqueL <bielghsilva@gmail.com>
2024-06-05 11:31:36 -0300

    frist commit

---
+++ aula.py
@@ -0,0 +1,6 @@
+nome = "Gabriel Henrique"
+idade = 16 
+altura = 1.69
+maior_de_idade = idade>=18
+print("Nome:", nome, "Idade:", idade, "Altura:", altura)
+print("É maior de idade?", maior_de_idade)
